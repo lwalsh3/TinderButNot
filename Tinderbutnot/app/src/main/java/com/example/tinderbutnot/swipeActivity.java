57f69c56fff7940ecc24ac765c2ade7c81c1dfb7
lwalsh3 <lwalsh2@ggc.edu>
2019-11-16 10:05:22 -0500

    button set up in swipe activity

--- Tinderbutnot/app/src/main/java/com/example/tinderbutnot/swipeActivity.java
+++ Tinderbutnot/app/src/main/java/com/example/tinderbutnot/swipeActivity.java
@@ -3,12 +3,53 @@ package com.example.tinderbutnot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class swipeActivity extends AppCompatActivity {
+
+    private Button dislikeButton;
+    private Button likeButton;
+    private Button saveImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
+
+        dislikeButton = findViewById(R.id.buttonDislike);
+        likeButton = findViewById(R.id.buttonLike);
+        saveImageButton = findViewById(R.id.buttonSaveImage);
+
+        dislikeButton.setOnClickListener((new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO decrement current Post score, set current Post userDislike to true, move on to next post
+            }
+        }));
+
+        likeButton.setOnClickListener((new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO increment current Post score, set current Post userlike to true, move on to next post
+            }
+        }));
+
+        saveImageButton.setOnClickListener((new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO save Image from current post to local storage of device
+            }
+        }));
     }
+
+
+
+
 }
